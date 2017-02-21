d17ef389c14659cc4123d4c5f215d369394f57d8
Benjamin Friesen <bfriesen95@gmail.com>
2017-02-21 11:31:56 -0800

    project setup

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,29 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
+# From: https://gist.github.com/PolBaladas/07bfcdefb5c1c57cdeb5
+
+from flask import Flask
+from flask import render_template
+from flask import request
+import models
+
+
 @app.route("/")
 def index():
     return Response("Hello World!"), 200
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def home():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        models.insertUser(username, password)
+        users = models.retrieveUsers()
+        return render_template('index.html', users=users)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
---
+++ project/models.py
@@ -0,0 +1,19 @@
+import sqlite3 as sql
+
+# From: https://gist.github.com/PolBaladas/07bfcdefb5c1c57cdeb5
+
+def insertUser(username,password):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
+    con.commit()
+    con.close()
+
+
+def retrieveUsers():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT username, password FROM users")
+    users = cur.fetchall()
+    con.close()
+    return users
