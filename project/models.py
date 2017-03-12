deda8db6c563eb54766dfbe8a794f1ade3353420
Benjamin Friesen <bfriesen95@gmail.com>
2017-03-12 16:22:28 -0700

    dup user check fix

--- project/models.py
+++ project/models.py
@@ -16,7 +16,7 @@ cur.execute(q)
 
 # Returns True if succesfully added
 def insertUser(username, password):
-    if getUser(username) is None:
+    if getUser(username) is not None:
         return False
     con = sql.connect("database.db")
     cur = con.cursor()
